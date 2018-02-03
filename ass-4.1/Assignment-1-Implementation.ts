f34cf9528fb943aad03564911653fac0db38ca86
jasmitha <jasmieyjas@gmail.com>
2018-02-03 22:32:03 +0530

    Add files via upload

---
+++ ass-4.1/Assignment-1-Implementation.ts
@@ -0,0 +1,8 @@
+/// < reference path="=Assignment1-Assignment-2-Interface.ts" />
+// defining the player details
+let playerDetail: PlayerNameSpace.Players = {
+  name: "Kohli",
+  jerseyNumber: 23,
+  fieldingPosition: "Gully"
+}
+PlayerNameSpace.playerPostion(playerDetail);
---
+++ ass-4.1/Assignment-1-Interface.ts
@@ -0,0 +1,9 @@
+
+// defining namespace
+namespace PlayerNameSpace {
+  export interface Players {
+    name: string;
+    jerseyNumber: number;
+    fieldingPosition: string;
+  } 
+}
